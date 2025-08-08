c8ce5a1db40d6ec424823cc3ffa44a2306b634d1
Qingkun Deng <106399042+Ericdqk001@users.noreply.github.com>
2025-08-08 14:30:22 +0100

    Integrate preprocess to main

---
+++ src/main.py
@@ -0,0 +1,77 @@
+import logging
+import shutil
+from pathlib import Path
+
+from preprocess.preprocess import preprocess
+
+
+def main(
+    data_store_path,
+    version_name: str = "",
+    experiment_number: int = 1,
+    # predictor: str = "",
+):
+    # Call the preprocess function from the prepare_img module
+    preprocess(
+        data_store_path=data_store_path,
+        version_name=version_name,
+        experiment_number=experiment_number,
+    )
+
+
+if __name__ == "__main__":
+    version_name = "test_2"
+
+    experiment_number = 1
+
+    data_store_path = Path(
+        "/",
+        "Volumes",
+        "GenScotDepression",
+    )
+
+    analysis_root_path = Path(
+        data_store_path,
+        "users",
+        "Eric",
+        "depression_trajectories",
+    )
+
+    experiments_path = Path(
+        analysis_root_path,
+        version_name,
+        "experiments",
+    )
+
+    if not experiments_path.exists():
+        experiments_path.mkdir(parents=True, exist_ok=True)
+
+    results_path = Path(
+        experiments_path,
+        f"exp_{experiment_number}",
+    )
+
+    if not results_path.exists():
+        results_path.mkdir(parents=True, exist_ok=True)
+
+    local_log_file = Path("/tmp") / "experiment.log"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.FileHandler(local_log_file), logging.StreamHandler()],
+    )
+
+    main(
+        data_store_path=data_store_path,
+        version_name=version_name,
+        experiment_number=experiment_number,
+    )
+
+    final_log_file = results_path / "experiment.log"
+
+    try:
+        shutil.move(str(local_log_file), str(final_log_file))
+        print(f"Log file moved to: {final_log_file}")
+    except Exception as e:
+        print(f"Failed to move log file to mounted drive: {e}")
